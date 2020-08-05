960239ad2d9d67eaa89e2ef10c11af2bb8f70208
David Hohengaßner <david@hohengassner.com>
2020-08-05 16:44:19 +0200

    🚚 move skipper ingress kustomization into kustomize dir
    This commit moves the skipper kustomization tested in skipper e2e tests.
    
    During startup Flagger throws an warning because Prometheus is not ready
    at this time.
    http://flagger-prometheus:9090 unreachable running query failed
    
    There is no need to fix this because it will retried with the Canary.
    https://github.com/weaveworks/flagger/issues/664

--- test/e2e-skipper.sh
+++ test/e2e-skipper.sh
@@ -4,18 +4,19 @@ set -o errexit
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-echo '>>> Installing Skipper Ingress'
-kustomize build test/skipper/ | kubectl apply -f -
-kubectl rollout status deployment/skipper-ingress -n kube-system
-kubectl get all --all-namespaces
-
 echo '>>> Loading Flagger image'
 kind load docker-image test/flagger:latest
 
-echo '>>> Installing Flagger'
-kubectl apply -k ${REPO_ROOT}/kustomize/kubernetes
+echo '>>> Installing Skipper Ingress, Flagger and Prometheus'
+# use kustomize to avoid compatibility issues:
+# https://github.com/kubernetes-sigs/kustomize/issues/2390
+# Skipper will throw an Prometheus warning which can be ignored:
+# https://github.com/weaveworks/flagger/issues/664
+kustomize build ${REPO_ROOT}/kustomize/skipper | kubectl apply -f -
+
+kubectl rollout status deployment/skipper-ingress -n kube-system
+kubectl rollout status deployment/flagger-prometheus -n flagger-system
 
 kubectl -n flagger-system set image deployment/flagger flagger=test/flagger:latest
 
 kubectl -n flagger-system rollout status deployment/flagger
-kubectl -n flagger-system rollout status deployment/flagger-prometheus
