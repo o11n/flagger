630b78768463132bbfc55912754b216a2d9874dd
slang <slang@bethel.jw.org>
2020-08-12 12:49:33 +0200

    refactor canary tests

--- test/e2e-skipper-tests.sh
+++ test/e2e-skipper-tests.sh
@@ -9,84 +9,17 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 echo '>>> Creating test namespace'
 kubectl create namespace test || true
-echo '>>> service canary'
-kubectl apply -f ${REPO_ROOT}/test/e2e-skipper-test-ingress.yaml
-echo '>>> Initialising canary'
+
+echo '>>> Initialising workload'
 kubectl apply -f ${REPO_ROOT}/test/e2e-workload.yaml
+kubectl apply -f ${REPO_ROOT}/test/e2e-workload-ingress.yaml
 
 echo '>>> Installing load tester'
 kubectl apply -k ${REPO_ROOT}/kustomize/tester
 kubectl -n test rollout status deployment/flagger-loadtester
 
 echo '>>> Create canary CRD'
-cat <<EOF | kubectl apply -f -
-apiVersion: flagger.app/v1beta1
-kind: Canary
-metadata:
-  name: podinfo
-  namespace: test
-spec:
-  provider: skipper
-  targetRef:
-    apiVersion: apps/v1
-    kind: Deployment
-    name: podinfo
-  ingressRef:
-    apiVersion: networking.k8s.io/v1beta1
-    kind: Ingress
-    name: podinfo-ingress
-  progressDeadlineSeconds: 60
-  service:
-    # service name (defaults to targetRef.name)
-    name: podinfo-service
-    # ClusterIP port number
-    port: 80
-    # container port name or number (optional)
-    targetPort: 9898
-    # port name can be http or grpc (default http)
-    portName: http
-    # add all the other container ports
-    # to the ClusterIP services (default false)
-    portDiscovery: false
-  analysis:
-    interval: 15s
-    threshold: 5
-    maxWeight: 100
-    stepWeight: 10
-    metrics:
-    - name: request-success-rate
-      interval: 15s
-      # minimum req success rate (non 5xx responses)
-      # percentage (0-100)
-      thresholdRange:
-        min: 99
-    - name: request-duration
-      interval: 15s
-      # maximum req duration P99
-      # milliseconds
-      thresholdRange:
-        max: 500
-    webhooks:
-      - name: gate
-        type: confirm-rollout
-        url: http://flagger-loadtester.test/gate/approve
-      - name: acceptance-test
-        type: pre-rollout
-        url: http://flagger-loadtester.test/
-        timeout: 10s
-        metadata:
-          type: bash
-          cmd: "curl -sd 'test' http://podinfo-service-canary/token | grep token"
-      - name: "load test"
-        type: rollout
-        url: http://flagger-loadtester.test/
-        timeout: 5s
-        metadata:
-          type: cmd
-          cmd: "hey -z 2m -q 10 -c 2 -host app.example.com http://skipper-ingress.kube-system"
-          logCmdOutput: "true"
-EOF
-
+kubectl apply -f ${REPO_ROOT}/test/e2e-skipper-canary.yaml
 echo '>>> Waiting for primary to be ready'
 retries=50
 count=0
