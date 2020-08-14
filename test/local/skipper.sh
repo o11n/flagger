42bb59a82cb43ec3a002118be3d767caf9c9da36
Samuel Lang <gh@lang-sam.de>
2020-08-14 12:57:32 +0200

    👷 Add high-level E2E test steps for Skipper
    Add e2e-skipper* files for test setup
    
    It does the following things:
    * install Skipper ingress with Kustomize
    * load Flagger image onto the local cluster
    * install Flagger and Prometheus in the flagger-system namespace

---
+++ test/local/skipper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd $REPO_ROOT
+
+# make test
+make build
+docker tag weaveworks/flagger:latest test/flagger:latest
+make loadtester-build
+(kind get clusters && kubectl delete ns/test --force) || kind create cluster --wait 5m --image kindest/node:v1.16.9
+./test/e2e-skipper.sh
+# port forward prometheus UI to localhost:9090
+kubectl port-forward $(kubectl get pods -l=app=flagger-prometheus -o name -n flagger-system | head -n 1) 9090:9090 -n flagger-system &
+
+./test/e2e-skipper-tests.sh
